af600a09b2d9041383108a3bcdf0adf59aefde14
Dmitry Neverov <dmitry.neverov@gmail.com>
2008-12-06 23:33:11 +0300

    move interface settings to nd-decorations

--- nd/nd-decorations.el
+++ nd/nd-decorations.el
@@ -1,5 +1,43 @@
 ;;;; look and feel
 
+;; no scrollbar
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;; no toolbar
+(if (fboundp 'tool-bar-mode)   (tool-bar-mode -1))
+;; no menu
+(if (fboundp 'menu-bar-mode)   (menu-bar-mode -1))
+
+;; maximum font decoration
+(setq font-lock-mode-maximum-decoration t)
+(global-font-lock-mode t)
+
+;; show marked text
+(setq transient-mark-mode 1)
+
+;; show parents
+(show-paren-mode 1)
+
+;; not blinking cursor 
+(blink-cursor-mode -1)
+
+;; tab settings
+(setq default-tab-width  2)
+;; insert space instead of tab
+(setq-default indent-tabs-mode nil) 
+
+;; show column number in status bar
+(setq column-number-mode t)
+
+;; empty scratch message
+(setq initial-scratch-message nil)
+(setq inhibit-splash-screen t)
+
+;; mark current line:
+(global-hl-line-mode 1)
+
+;; color for current line:
+(set-face-background 'hl-line "#e0f8ff")
+
 (load-library "time")
 (setq display-time-24hr-format t
       display-time-mail-file t
