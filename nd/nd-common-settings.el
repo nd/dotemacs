ead1cbcd659c6af854cef26cdb782a90dcae5ee7
Dmitry Neverov <dmitry.neverov@gmail.com>
2008-12-07 11:45:11 +0300

    bury *scratch* buffer instead of kill it

--- nd/nd-common-settings.el
+++ nd/nd-common-settings.el
@@ -30,5 +30,12 @@
       kept-new-versions 4
       kept-old-versions 2)
 
+;; bury *scratch* buffer instead of kill it
+(defadvice kill-buffer (around kill-buffer-around-advice activate)
+  (let ((buffer-to-kill (ad-get-arg 0)))
+    (if (equal buffer-to-kill "*scratch*")
+        (bury-buffer)
+      ad-do-it)))
+
 
 (provide 'nd-common-settings)
