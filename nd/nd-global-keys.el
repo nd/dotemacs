dfb6072b6dd801a0749e26e85d1b1e1e5778c475
Dmitry Neverov <dmitry.neverov@gmail.com>
2008-12-09 21:51:32 +0300

    add cua mode setting for rectangle editing

--- nd/nd-global-keys.el
+++ nd/nd-global-keys.el
@@ -27,5 +27,11 @@
 ;; scroll up
 (global-set-key "\C-\M-z" '(lambda () (interactive) (scroll-down 1)))
 
+;; cua is usefull for rectagle editing
+(setq cua-enable-cua-keys nil)
+(setq cua-highlight-region-shift-only nil) ;no transient mark mode
+(setq cua-toggle-set-mark nil) ;original set-mark behavior, i.e. no transient-mark-mode
+(cua-mode)
+
 
 (provide 'nd-global-keys)
