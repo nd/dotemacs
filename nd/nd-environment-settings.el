6bf778f30de476fc9afde11f05419e17ce7ca091
Dmitry Neverov <dmitry.neverov@gmail.com>
2009-01-09 17:51:25 +0300

    add encoding settings

---
+++ nd/nd-environment-settings.el
@@ -0,0 +1,15 @@
+;;;; environment settings
+
+(set-language-environment 'Russian)
+
+(if (equal system-type 'gnu/linux)
+    (progn (set-default-coding-systems 'utf-8-unix)
+           (set-buffer-file-coding-system 'utf-8-unix)
+           (prefer-coding-system 'utf-8-unix)))
+
+(if (equal system-type 'ms-dos)
+    (progn (set-default-coding-systems 'windows-1251-dos)
+           (set-buffer-file-coding-system 'windows-1251-dos)
+           (prefer-coding-system 'windows-1251-dos)))
+
+(provide 'nd-environment-settings)
