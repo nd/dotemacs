3c2aadf97db602dfc0568f1fb48e96bc433f20a4
Dmitry Neverov <dmitry.neverov@gmail.com>
2009-04-20 23:09:07 +0400

    now I get all information from port to message we want to request

--- nd/wsdl/src/wsdl2.el
+++ nd/wsdl/src/wsdl2.el
@@ -3,10 +3,10 @@
 (require 'cl)
 
 (defstruct source
-  xml           ;;result of parsing using nxml-parse-file
-  tns           ;;targetNamespace of document
-  aliases       ;;alist prefix->namespace
-  used-sources  ;;alist namespace->location-or-node
+  xml           ;; result of parsing using nxml-parse-file
+  tns           ;; targetNamespace of document
+  aliases       ;; alist prefix->namespace
+  used-sources  ;; alist namespace->location-or-node
   location)
  
 
@@ -15,39 +15,47 @@
   "Build soap request to one of wsdl's operations in separate buffer. second version"
   (interactive "swsdl: ")
   (with-temp-buffer
-    (make-local-variable 'wsdl-location)
-    (setq wsdl-location wsdl-location)
-
+    (make-local-variable 'wsdl-location)  ;; wsdl-location
     (make-local-variable 'loaded-sources) ;; location->source
+    (make-local-variable 'tns->source)    ;; map targetNamespace of source -> source
+    (make-local-variable 'port)           ;; port we request
+    (make-local-variable 'port-location)  ;; location of port we request
+    (make-local-variable 'current-source) ;; xml-source we currently work with
+    (make-local-variable 'binding)        ;; binding of port
+    (make-local-variable 'portType)       ;; portType we request
+    (make-local-variable 'operation)      ;; operation we request
+    (make-local-variable 'message)        ;; input message of operation we request
+
+
+    (setq wsdl-location wsdl-location)
     (setq loaded-sources (make-hash-table))
-
-    (make-local-variable 'tns->source)
     (setq tns->source (make-hash-table))
-
-    (make-local-variable 'wsdl)
-    (setq wsdl (register-source-maybe wsdl-location))
+    (setq current-source (register-source-maybe wsdl-location))
 
     ;; choose port:
-    (make-local-variable 'port)
     (setq port (util/select
-                (wsdl2/get-ports (source-xml wsdl))
+                (wsdl2/get-ports (source-xml current-source))
                 (lambda (port-node) (xml/get-attribute-value port-node "name"))
                 "Port: "))
-
-    (make-local-variable 'port-location)
     (setq port-location (wsdl2/get-port-location port))
 
-    (let ((binding-name (wsdl2/get-port-binding port (source-tns wsdl))))
-      (wsdl2/resolve-ns wsdl (nxml-get-namespace binding-name))
-      (make-local-variable 'binding)
-      (setq binding (wsdl2/get-binding binding-name))
+    (let ((binding-name (wsdl2/get-port-binding port (source-tns current-source))))
+      (setq current-source (wsdl2/resolve-ns current-source (nxml-get-namespace binding-name)))
+      (setq binding (wsdl2/get-binding binding-name)))
 
-      (let ((portType-name (wsdl2/get-binding-portType binding (source-tns wsdl))))
-        (wsdl2/resolve-ns wsdl (nxml-get-namespace portType-name))
-        (make-local-variable 'portType)
-        (setq portType (wsdl2/get-portType portType-name))))
+    (let ((portType-name (wsdl2/get-binding-portType binding (source-tns current-source))))
+      (setq current-source (wsdl2/resolve-ns current-source (nxml-get-namespace portType-name)))
+      (setq portType (wsdl2/get-portType portType-name)))
     
-    ;; choose operation:
+    (setq operation (util/select
+                     (wsdl2/get-operations portType)
+                     (lambda (operation-node) (xml/get-attribute-value operation-node "name"))
+                     "Operation: "))
+
+    (let* ((input (wsdl2/get-input operation))
+           (message-name (wsdl2/get-input-message input (source-tns current-source))))
+      (setq current-source (wsdl2/resolve-ns current-source (nxml-get-namespace message-name)))
+      (setq message (wsdl2/get-message message-name)))
 
     ;; build request
     (concat "soap request to " (xml/get-attribute-value portType "name"))))
@@ -155,11 +163,36 @@ SOURCE is xml source that use this namespace."
       (setf (source-xml source) (xml/parse-document-at-location location))
       (setf (source-tns source) (nxml-make-namespace (xml/get-attribute-value (source-xml source) "targetNamespace")))
       (setf (source-aliases source) (xml/get-ns-aliases2 (source-xml source)))
-      (setf (source-used-sources source) (wsdl2/get-used-sources (source-xml source) wsdl-location))
+      (setf (source-used-sources source) (wsdl2/get-used-sources (source-xml source) location))
       (setf (source-location source) location)
-      (puthash wsdl-location source loaded-sources)
+      (puthash location source loaded-sources)
       (puthash (source-tns source) source tns->source)
       source)))
 
+(defun wsdl2/get-operations (portType)
+  (xml/get-elements-by-name portType '(:http://schemas.xmlsoap.org/wsdl/ . "operation")))
+
+(defun wsdl2/get-input (operation)
+  (let ((inputs (xml/get-elements-by-name operation '(:http://schemas.xmlsoap.org/wsdl/ . "input"))))
+    (if (null inputs)
+        (error "operation has no input")
+      (car inputs))))
+
+(defun wsdl2/get-input-message (input tns)
+  (let ((string-value (xml/get-attribute-value input "message"))
+        (ns-aliases (source-aliases (gethash tns tns->source))))
+    (xml/expand-qname2 string-value tns ns-aliases)))
+
+(defun wsdl2/get-messages (wsdl)
+  "Get list of message nodes from wsdl. Wsdl is a result of nxml-parse-file."
+  (xml/get-elements-by-name wsdl '(:http://schemas.xmlsoap.org/wsdl/ . "message")))
+
+(defun wsdl2/get-message (message-name)
+  "Get message node by message-name"
+  (let ((wsdl (source-xml (gethash (nxml-get-namespace message-name) tns->source))))
+    (find (nxml-get-localname message-name) 
+          (wsdl2/get-messages wsdl)
+          :test (lambda (name message-node)
+                  (equal (xml/get-attribute-value message-node "name") name)))))
 
 (provide 'wsdl2)
