acd603fd0c628082e21425a0740babf27b4a16e7
nd <neverov@bercut.com>
2009-04-20 09:30:56 +0400

    add command go to sles dir

--- work/nd-sles.el
+++ work/nd-sles.el
@@ -1,13 +1,20 @@
 ;;;; util for working with sles
 
+(defconst sles-dir "d:/Workspace/Bercut/SDEP/SLES")
+
+
 (defun nd-delete-deploy-dirs ()
   (interactive)
-  (dired-delete-file "deployed" 'always)
-  (dired-delete-file "httpTmp" 'always)
-  (dired-delete-file "shared_classes" 'always)
-  (revert-buffer nil t))
+  (message "cleaning sles")
+  (ignore-errors (dired-delete-file "deployed" 'always))
+  (ignore-errors (dired-delete-file "test" 'always))
+  (ignore-errors (dired-delete-file "tmp" 'always))
+  (ignore-errors (dired-delete-file "shared_classes" 'always))
+  (revert-buffer nil t)
+  (message "cleaning is finished"))
 
 (define-key dired-mode-map "\C-csc" 'nd-delete-deploy-dirs)
+(global-set-key "\C-csd" '(lambda () (interactive) (dired sles-dir)))
 
 
 (provide 'nd-sles)
