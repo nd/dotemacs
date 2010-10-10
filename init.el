340d08589b5a4b697276370ac2eef1c3472975d9
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-10-10 12:01:38 +0400

    Load things that should work without 3rd party code first
    So basic functionality will always work

--- init.el
+++ init.el
@@ -1,28 +1,27 @@
-
 ;; entry point to my emacs customizations
 
 (add-to-list 'load-path "~/.emacs.d/nd")
 
+;;settings that require no 3rd party code, should work with plain emacs:
 (require 'nd-common-settings)
 (require 'nd-global-keys)
-
 (require 'nd-decorations)
 (require 'nd-ido-settings)
-;(require 'nd-emms-settings)
 (require 'nd-shell-settings)
-(require 'nd-git-settings)
-;(require 'nd-dired-settings)
+(require 'nd-dired-settings)
 (require 'nd-woman-settings)
 (require 'nd-environment-settings)
 (require 'nd-nxml-settings)
-(require 'nd-scheme-settings)
-(require 'nd-server-settings)
 (require 'nd-gpg-settings)
+(require 'nd-server-settings)
+(require 'nd-startup-settings)
+
+(require 'nd-git-settings)
+;(require 'nd-emms-settings)
+(require 'nd-scheme-settings)
 (require 'nd-openwith-settings)
 (require 'nd-auto-complete-settings)
-(require 'nd-dbus-settings)
-(require 'nd-elib-settings)
-(require 'nd-startup-settings)
+;(require 'nd-dbus-settings)
 ;(require 'nd-wsdl-settings)
 (require 'nd-clojure-settings)
 (require 'nd-haskell-settings)
