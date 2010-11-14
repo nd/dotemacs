65748537c63be72f88ae1ff38b32828e168a32b1
Dmitry Neverov <dmitry.neverov@gmail.com>
2010-11-14 21:28:41 +0300

    I rarely need it

--- nd/nd-openwith-settings.el
+++ nd/nd-openwith-settings.el
@@ -8,7 +8,8 @@
         ("\\.\\(?:mpe?g\\|avi\\|wmv\\)\\'" "mplayer" ("-idx" file))
         ("\\.\\(?:jp?g\\|png\\)\\'" "display" (file))
         ("\\.chm\\'" "xchm" (file))
-        ("\\.html?\\'" "firefox" (file))))
+        ;("\\.html?\\'" "firefox" (file))
+        ))
 
 
 (provide 'nd-openwith-settings)
