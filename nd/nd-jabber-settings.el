93b484aad6d0f38eb69544d06eb8f809d0f93fab
Dmitry Neverov <dmitry.neverov@gmail.com>
2011-11-10 19:34:23 +0400

    jabber settings

---
+++ nd/nd-jabber-settings.el
@@ -0,0 +1,45 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-jabber")
+
+(require 'jabber)
+
+(setq jabber-account-list
+      '(("dmitry.neverov@gmail.com" 
+         (:network-server . "talk.google.com")
+         (:connection-type . ssl))))
+
+(setq
+  jabber-history-enabled t
+  jabber-use-global-history nil
+  jabber-backlog-number 40
+  jabber-backlog-days 30)
+
+(define-key jabber-chat-mode-map (kbd "RET") 'newline)
+(define-key jabber-chat-mode-map [M-return] 'jabber-chat-buffer-send)
+
+(add-hook 'jabber-chat-mode-hook 'goto-address)
+
+(setq jabber-alert-presence-message-function 
+      '(lambda (who oldstatus newstatus statustext) nil))
+
+(defvar libnotify-program "/usr/bin/notify-send")
+
+(defun notify-send (title message)
+  (start-process "notify" " notify"
+		 libnotify-program "--expire-time=4000" title message))
+
+(defun libnotify-jabber-notify (from buf text proposed-alert)
+  "(jabber.el hook) Notify of new Jabber chat messages via libnotify"
+  (when (or jabber-message-alert-same-buffer
+            (not (memq (selected-window) (get-buffer-window-list buf))))
+    (if (jabber-muc-sender-p from)
+        (notify-send (format "(PM) %s"
+                       (jabber-jid-displayname (jabber-jid-user from)))
+               (format "%s: %s" (jabber-jid-resource from) text)))
+      (notify-send (format "%s" (jabber-jid-displayname from))
+             "")))
+
+(add-hook 'jabber-alert-message-hooks 'libnotify-jabber-notify)
+
+(setq jabber-vcard-avatars-retrieve nil)
+
+(provide 'nd-jabber-settings)
