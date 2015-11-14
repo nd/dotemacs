cf69c96788679d5316c16a50d9560c76b378a469
Dmitry Neverov <dmitry.neverov@gmail.com>
2015-11-14 09:33:12 +0100

    fix encoding

--- nd/nd-environment-settings.el
+++ nd/nd-environment-settings.el
@@ -2,14 +2,15 @@
 
 (set-language-environment 'Russian)
 
-(if (equal system-type 'gnu/linux)
+(if (or (equal system-type 'gnu/linux)
+        (equal system-type 'darwin))
     (progn (set-default-coding-systems    'utf-8-unix)
            (set-buffer-file-coding-system 'utf-8-unix)
            (prefer-coding-system          'utf-8-unix)))
 
 (if (equal system-type 'windows-nt)
-    (progn (set-default-coding-systems    'windows-1251-dos)
-           (set-buffer-file-coding-system 'windows-1251-dos)
-           (prefer-coding-system          'windows-1251-dos)))
+    (progn (set-default-coding-systems    'utf-8-dos)
+           (set-buffer-file-coding-system 'utf-8-dos)
+           (prefer-coding-system          'utf-8-dos)))
 
 (provide 'nd-environment-settings)
