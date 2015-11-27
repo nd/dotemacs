6b142e7716fabf75c645e8c49873cec7664afdac
Dmitry Neverov <dmitry.neverov@gmail.com>
2015-11-27 20:26:17 +0100

    disable desktop-mode

--- nd/nd-common-settings.el
+++ nd/nd-common-settings.el
@@ -50,10 +50,6 @@
              (or (file-exists-p (file-name-directory buffer-file-name))
                  (make-directory (file-name-directory buffer-file-name) t))))
 
-;; session settings
-(desktop-save-mode 1)
-(setq desktop-save t)
-(add-to-list 'desktop-modes-not-to-save 'dired-mode)
 ;; save minibuffer history between sessions
 (savehist-mode 1)
 
